3419e6f48ff4086cee7e38fc94098c0ddc757bde
tazrian-amin <amin.tazrian1979@gmail.com>
2022-10-20 05:56:37 +0600

    rearranged tshirt.json file and updated styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,4 @@
+import logo from '../../logo.png';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import '../Header/Header.css';
@@ -5,9 +6,14 @@ import '../Header/Header.css';
 const Header = () => {
     return (
         <div className='header'>
-            <NavLink className='nav-link' to="/">Home</NavLink>
-            <NavLink className='nav-link' to="/shop">Shop</NavLink>
-            <NavLink className='nav-link' to="/contact">Contact</NavLink>
+            <div className='logo-container'>
+                <img src={logo} alt="logo" />
+            </div>
+            <div>
+                <NavLink className='nav-link' to="/">Home</NavLink>
+                <NavLink className='nav-link' to="/shop">Shop</NavLink>
+                <NavLink className='nav-link' to="/contact">Contact</NavLink>
+            </div>
         </div>
     );
 };
